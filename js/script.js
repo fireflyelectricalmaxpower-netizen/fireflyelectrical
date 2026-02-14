670607a00d1cf659da094f7e51935482a83e40c0
fireflyelectricalmaxpower-netizen <fireflyelectrical.maxpower@gmail.com>
2026-02-14 10:25:14 +0530

    Update script.js
    led

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ startInterval();
 
     // Wait so browser shows it
     setTimeout(() => {
-      window.location.href = "indexled.html";
+      window.location.href = "led-bulbs.html";
     }, 1500);
 
   });
@@ -314,3 +314,4 @@ function handleSwipe() {
 
   window.addEventListener("scroll", revealSections);
   window.addEventListener("load", revealSections);
+
